814b836354bb54633f12e06e37c7049b17c59520
gonzalobenini <95836577+gonzalobenini@users.noreply.github.com>
2023-02-07 15:39:29 -0300

    Update settings.py

--- chessbe/settings.py
+++ chessbe/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-+bgqyhc@!*5%a34aq_dvuoxr5*cs@k#t&6#w9%#q(e2uk_^ezt
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
